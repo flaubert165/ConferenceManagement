ac2970133fb8c3e6fc61093e01776b8fae9ce096
italosantana <flaubert165@gmail.com>
2018-05-26 18:34:46 -0300

    hot fixes

---
+++ ConferenceManagement.Application/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using ConferenceManagement.Common.Validations;
+using ConferenceManagement.Common.Resources;
+using ConferenceManagement.Domain.Services;
+
+namespace ConferenceManagement.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Directory.GetCurrentDirectory() + "/InputData/TestInput.txt";
+
+            Guard.ForNullOrEmpty(file, Messages.FileNotFound);
+
+            try
+            {
+                Startup.ServiceProvider.GetService<IPrintService>().PrintInitialMessage();
+
+                var talks = Startup.ServiceProvider
+                                    .GetService<ITalkService>()
+                                    .GetTalksFromTextFile(file);
+
+                Guard.IEnumerableIsNullOrEmpty(talks);
+
+                var conference = Startup
+                                    .ServiceProvider
+                                    .GetService<IConferencePlanningService>()
+                                    .GreedyBestFitApproach(talks);
+
+
+                Startup.ServiceProvider.GetService<IPrintService>().PrintResult(conference);
+
+                Startup.ServiceProvider.GetService<IPrintService>().PrintEndMessage();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
